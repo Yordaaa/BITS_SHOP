dd493d14e4630b8570bc7a2bbf21c0ffb721b081
YerosenHunduma <yerosenhunduma@gmail.com>
2024-09-15 17:28:42 +0300

    integrate seller bid offer page

--- src/pages/Bids.tsx
+++ src/pages/Bids.tsx
@@ -1,13 +1,15 @@
-import { useParams } from 'react-router-dom';
+import { ErrorResponse, useParams } from 'react-router-dom';
 import SideNav from '../components/SideNav';
-import { useSellerGetBidRequestQuery } from '../redux/Features/bidApiSlice';
+import { useBidAcceptMutation, useBidRejectMutation, useSellerGetBidRequestQuery } from '../redux/Features/bidApiSlice';
 import { useState } from 'react';
+import { RegistrationResponseProps } from '../redux/Features/types';
+import { toast } from 'react-toastify';
 
 function Bids() {
     const { id } = useParams();
     const { data: bids, isLoading } = useSellerGetBidRequestQuery(id);
     const [expandedBids, setExpandedBids] = useState(new Set());
-
+    console.log(bids);
     const toggleBid = (bidId: unknown) => {
         const newExpandedBids = new Set(expandedBids);
         if (newExpandedBids.has(bidId)) {
@@ -16,6 +18,39 @@ function Bids() {
             newExpandedBids.add(bidId);
         }
         setExpandedBids(newExpandedBids);
+    };
+
+    const [bidReject] = useBidRejectMutation();
+    const [bidAccept] = useBidAcceptMutation();
+
+    const handleBidReject = async (bidId: String) => {
+        try {
+            const res = await bidReject(bidId);
+            if ('data' in res) {
+                const { data } = res as { data: RegistrationResponseProps };
+                toast.success(data.message);
+            } else {
+                const { error } = res as { error: ErrorResponse };
+                toast.error(error.data.message);
+            }
+        } catch (error) {
+            toast.error('Unexpected error occurred');
+        }
+    };
+
+    const handleBidAccept = async (bidId: String) => {
+        try {
+            const res = await bidAccept(bidId);
+            if ('data' in res) {
+                const { data } = res as { data: RegistrationResponseProps };
+                toast.success(data.message);
+            } else {
+                const { error } = res as { error: ErrorResponse };
+                toast.error(error.data.message);
+            }
+        } catch (error) {
+            toast.error('Unexpected error occurred');
+        }
     };
 
     return (
@@ -38,12 +73,12 @@ function Bids() {
                             <p className="mt-4 text-gray-600">There are no bids for this product.</p>
                         </div>
                     ) : (
-                        bids.map((bid) => (
+                        bids?.map((bid) => (
                             <div key={bid._id} className="bg-white shadow-lg rounded-lg p-6 border border-gray-200">
                                 <div className="flex justify-between items-center mb-4">
                                     <div>
                                         <p className="text-lg font-bold">Product ID:</p>
-                                        <p className="text-gray-600">{bid.productId}</p>
+                                        <p className="text-gray-600 truncate max-w-[100px] whitespace-nowrap overflow-hidden text-ellipsis">{bid.productId}</p>
                                     </div>
                                     <div>
                                         <p className="text-lg font-bold">Status:</p>
@@ -60,21 +95,42 @@ function Bids() {
                                         <p className="text-gray-600">{new Date(bid.createdAt).toLocaleDateString()}</p>
                                     </div>
                                 </div>
-                                <button
-                                    className="mt-4 text-blue-500 hover:underline"
-                                    onClick={() => toggleBid(bid._id)}
-                                >
+                                <button className="mt-4 text-blue-500 hover:underline" onClick={() => toggleBid(bid._id)}>
                                     {expandedBids.has(bid._id) ? 'Show Less' : 'Show More'}
                                 </button>
-
                                 {expandedBids.has(bid._id) && (
                                     <div className="mt-2 p-4 bg-gray-50 rounded-lg">
-                                        <p className="text-lg font-bold">Message:</p>
-                                        <p className="text-gray-600">{bid.message || 'N/A'}</p>
-                                        <div className="flex justify-end mt-4 space-x-2">
-                                            <button className="bg-primary text-white font-medium py-1 px-3 rounded-3xl flex items-center hover:bg-opacity-70">Accept</button>
-                                            <button className="bg-[#E3A57F] text-white font-medium py-1 px-3 rounded-3xl flex items-center hover:bg-opacity-70">Reject</button>
-                                        </div>
+                                        {bid.status === 'accepted' ? (
+                                            <div>
+                                                <p className="text-gray-600">{bid.message || 'N/A'}</p>
+
+                                                <p className="text-lg font-bold text-green-600">Bid Accepted</p>
+                                            </div>
+                                        ) : bid.status === 'rejected' ? (
+                                            <div>
+                                                <p className="text-gray-600">{bid.message || 'N/A'}</p>
+                                                <p className="text-lg font-bold text-red-600">Bid Rejected</p>
+                                            </div>
+                                        ) : (
+                                            <div>
+                                                <p className="text-lg font-bold">Message:</p>
+                                                <p className="text-gray-600">{bid.message || 'N/A'}</p>
+                                                <div className="flex justify-end mt-4 space-x-2">
+                                                    <button
+                                                        onClick={() => handleBidAccept(bid._id)}
+                                                        className="bg-primary text-white font-medium py-1 px-3 rounded-3xl flex items-center hover:bg-opacity-70"
+                                                    >
+                                                        Accept
+                                                    </button>
+                                                    <button
+                                                        onClick={() => handleBidReject(bid._id)}
+                                                        className="bg-[#E3A57F] text-white font-medium py-1 px-3 rounded-3xl flex items-center hover:bg-opacity-70"
+                                                    >
+                                                        Reject
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        )}
                                     </div>
                                 )}
                             </div>
